c7173a648491f9571dc7fa0348d6dd61bb84509e
Lamartine Teixeira <lamartine.teixeira@gmail.com>
2021-08-17 21:10:24 -0300

    sadasd

--- portfolio/src/importacao_dados.py
+++ portfolio/src/importacao_dados.py
@@ -1,10 +1,13 @@
 import pandas_datareader.data as pdr
+import os.path as path
+
+dir_base = path.dirname(__file__)
 
 # a partir de janeiro de 2018
 #ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "LTC-USD"]
 
 # a partir de janeiro de 2021
-ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "LTC-USD", "XRP-USD", "SOL1-USD", "DOT1-USD", "BCH-USD", "LUNA1-USD", "DOGE-USD"]
+ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "LTC-USD", "XRP-USD", "SOL1-USD", "DOT1-USD", "BCH-USD"]
 
 # a partir de abril de 2021
 #["UNI3-USD", "CAKE-USD", "MATIC-USD"]
@@ -13,4 +16,4 @@ fim = "2021-08-15"
 
 dados = pdr.get_data_yahoo(ativos, inicio, fim)
 dados = dados["Close"]
-dados.to_csv("../dados/cotacoes.csv")
+dados.to_csv( path.join(dir_base, '../dados/cotacoes.csv') )
