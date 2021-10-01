dfe58e84488ac88360c6f876c8c4d4f1f4c07b29
Lamartine Teixeira <lamartine.teixeira@gmail.com>
2021-10-01 11:25:58 -0300

    fdfdfdfdf

--- portfolio/src/importacao_dados.py
+++ portfolio/src/importacao_dados.py
@@ -1,7 +1,16 @@
 import pandas_datareader.data as pdr
 import os.path as path
 
-dir_base = path.dirname(__file__)
+def importar_dados(ativos, inicio, fim, nome_arquivo):
+    dir_base = path.dirname(__file__)
+    dados = pdr.get_data_yahoo(ativos, inicio, fim)
+    dados = dados["Close"]
+    dados.to_csv( path.join(dir_base, nome_arquivo) )
+
+# BTC a partir de 2018
+ativos = ["BTC-USD"]
+importar_dados(ativos, "2018-01-01", "2021-09-26", "../dados/btc-2018.csv")
+
 
 # a partir de janeiro de 2018
 #ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "LTC-USD"]
@@ -10,20 +19,6 @@ dir_base = path.dirname(__file__)
 # ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "LTC-USD", "XRP-USD", "SOL1-USD", "DOT1-USD", "BCH-USD"]
 
 # a partir de abril de 2021
-ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "SOL1-USD", "DOT1-USD", "UNI3-USD", "LUNA1-USD", "AVAX-USD", "ALGO-USD", "ATOM1-USD", "EGLD-USD", "AAVE-USD", "COMP-USD"]
+#ativos = ["BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD", "LINK-USD", "SOL1-USD", "DOT1-USD", "UNI3-USD", "LUNA1-USD", "AVAX-USD", "ALGO-USD", "ATOM1-USD", "EGLD-USD", "AAVE-USD", "COMP-USD"]
 
-
-# BTC a partir de dezembro de 2017
-# ativos = ["BTC-USD"]
-
-
-# a partir de abril de 2021
-#["UNI3-USD", "CAKE-USD", "MATIC-USD"]
-# inicio = "2021-01-01"
-# fim = "2021-08-15"
-inicio = "2021-04-01"
-fim = "2021-09-26"
-
-dados = pdr.get_data_yahoo(ativos, inicio, fim)
-dados = dados["Close"]
-dados.to_csv( path.join(dir_base, '../dados/cotacoes-2021-abril-setembro.csv') )
+# S&P500 a partir de 2018    ^GSPC
